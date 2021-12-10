5646198f3a2fc5c1c0fe1afd898adbadaca6bfbb
jadatix <andrijlasenko@gmail.com>
2021-12-10 21:50:25 +0200

    insert sells func with calculating

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace MyNamespace
             var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
 
             IMongoDatabase db = dbClient.GetDatabase("grocery");
-            InsertReceiptGoods(db);
-            ShowCollection("receipt_goods", db);
+            InsertSells(db);
+            ShowCollection("sells", db);
 
         }
 
@@ -59,6 +59,45 @@ namespace MyNamespace
             var doc = new BsonDocument { { "goods", id }, { "date", DateTime.UtcNow }, { "price_per_good", Int64.Parse(ppg) } };
             InsertToCollection("receipt_goods", db, doc);
         }
+        static void InsertSells(IMongoDatabase db){
+            var goods_col = db.GetCollection<BsonDocument>("goods");
+            var all_docs = goods_col.Find(new BsonDocument()).ToList();
+            ShowCollection("goods", db);
+            Console.WriteLine("ВВедіть \"name\" для якого товару даний продаж");
+            string? name;
+            ObjectId id = new ObjectId();
+            bool flag = true;
+            while (flag)
+            {
+                name = Console.ReadLine();
+                all_docs.ForEach(doc =>
+                    {
+                        if (doc.GetValue("name") == name)
+                        {
+                            id = ObjectId.Parse(doc.GetValue("_id").ToString());
+                            flag = false;
+                        }
+                    }
+                );
+
+            }
+            Int64 price_per_good = 0;
+            var _col = db.GetCollection<BsonDocument>("receipt_goods");
+            var all = _col.Find(new BsonDocument()).ToList();
+            all.ForEach(doc =>
+            {
+                if(doc.GetValue("goods")==id)
+                    price_per_good = doc.GetValue("price_per_good").ToInt64();
+            });
+            Console.WriteLine("Введіть кількісь поданих товарів");
+            string? sold = Console.ReadLine();
+            var doc = new BsonDocument { { "goods", id }, { "month", DateTime.UtcNow.Month }, 
+                                        {"sold",Int64.Parse(sold) },
+                                        {"earned_money",Int64.Parse(sold)*price_per_good} };
+            InsertToCollection("sells", db, doc);
+        }
+        
+
         static void InsertToCollection(string col_name, IMongoDatabase db, BsonDocument doc)
         {
             var col = db.GetCollection<BsonDocument>(col_name);
